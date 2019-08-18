5abc36a892cb5326957109d3463d57cb45c6b0cc
Ari Stathopoulos <aristath@gmail.com>
2019-08-18 13:36:15 +0300

    breadcrumbs improvements for latest gridd changes

--- inc/classes/Customizer.php
+++ inc/classes/Customizer.php
@@ -43,6 +43,7 @@ class Customizer {
 	public function include_files() {
 		require_once GRIDD_PLUS_PATH . '/inc/customizer/controls/kirki-wcag-tc/kirki-wcag-tc.php';
 		require_once GRIDD_PLUS_PATH . '/inc/customizer/controls/kirki-wcag-lc/kirki-wcag-lc.php';
+		require_once GRIDD_PLUS_PATH . '/inc/customizer/breadcrumbs.php';
 		require_once GRIDD_PLUS_PATH . '/inc/customizer/features.php';
 		require_once GRIDD_PLUS_PATH . '/inc/customizer/footer.php';
 		require_once GRIDD_PLUS_PATH . '/inc/customizer/grid.php';
