6f0de14ede4d9b4a917d8c475a2010eae9150ea9
Ari Stathopoulos <aristath@gmail.com>
2019-10-26 17:29:57 +0300

    use vars here, they make more sense

--- assets/js/offcanvas.js
+++ assets/js/offcanvas.js
@@ -1,8 +1,7 @@
-document.getElementById( 'offcanvas-wrapper' ).addEventListener( 'focusout', function( e ) {
-    if (
-        ! document.getElementById( 'offcanvas-wrapper' ).contains( e.relatedTarget ) &&
-        document.querySelector( '.toggle-gridd-plus-offcanvas-sidebar' ).classList.contains( 'toggled-on' )
-    ) {
-        document.querySelector( '.toggle-gridd-plus-offcanvas-sidebar' ).click();
+var griddOffConvasWrapper = document.getElementById( 'offcanvas-wrapper' );
+var griddOffCanvasTrigger = document.querySelector( '.toggle-gridd-plus-offcanvas-sidebar' );
+griddOffConvasWrapper.addEventListener( 'focusout', function( e ) {
+    if ( ! griddOffConvasWrapper.contains( e.relatedTarget ) && griddOffCanvasTrigger.classList.contains( 'toggled-on' ) ) {
+        griddOffCanvasTrigger.click();
     }
 });
