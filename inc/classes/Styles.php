034fb3f3bc64d9c99b4bf2a7a6defa6adbee7d91
Ari Stathopoulos <aristath@gmail.com>
2019-11-03 01:24:40 +0200

    cleanup for nav styles

--- inc/classes/Styles.php
+++ inc/classes/Styles.php
@@ -73,40 +73,6 @@ class Styles {
 				$style->add_file( GRIDD_PLUS_PATH . '/assets/css/nav-handheld-woo-cart.min.css' );
 				break;
 
-			case ( 0 === strpos( $style->context, 'grid-part/navigation/' ) ):
-				$nav_id                = str_replace( 'grid-part/navigation/', '', $style->context );
-				$responsive_mode       = get_theme_mod( "gridd_grid_nav_{$nav_id}_responsive_behavior", 'desktop-normal-mobile-hidden' );
-				$added_vertical_styles = false;
-				$style->add_file( GRIDD_PLUS_PATH . '/assets/css/navigation-main.min.css' );
-				if ( false !== strpos( $responsive_mode, 'desktop-normal' ) ) {
-					if ( get_theme_mod( "gridd_grid_nav_{$nav_id}_vertical", false ) ) {
-						$style->add_file( GRIDD_PLUS_PATH . '/assets/css/navigation-vertical.min.css' );
-						$added_vertical_styles = true;
-					}
-				}
-
-				if ( false !== strpos( $responsive_mode, 'icon' ) ) {
-					if ( false !== strpos( $responsive_mode, 'desktop-normal' ) ) {
-						$style->add_string( '@media only screen and (max-width:' . get_theme_mod( 'gridd_mobile_breakpoint', '800px' ) . '){' );
-					}
-					$style->add_file( GRIDD_PLUS_PATH . '/assets/css/navigation-collapsed.min.css' );
-					if ( ! $added_vertical_styles ) {
-						$style->add_file( GRIDD_PLUS_PATH . '/assets/css/navigation-vertical.min.css' );
-					}
-					if ( false !== strpos( $responsive_mode, 'desktop-normal' ) ) {
-						$style->add_string( '}' );
-					}
-				}
-				$style->replace( 'ID', $nav_id );
-				$style->add_vars(
-					[
-						"--nv-$nav_id-fs"  => get_theme_mod( "gridd_grid_nav_{$nav_id}_font_size", 1 ) . 'em',
-						"--nv-$nav_id-ipd" => get_theme_mod( "gridd_grid_nav_{$nav_id}_items_padding", 1 ) . 'em',
-						"--nv-$nav_id-cis" => get_theme_mod( "gridd_grid_nav_{$nav_id}_collapse_icon_size", 1 ) . 'em',
-					]
-				);
-				break;
-
 			case 'footer-late-styles':
 				$style->add_file( GRIDD_PLUS_PATH . '/assets/css/posts-and-pages.min.css' );
 		}
